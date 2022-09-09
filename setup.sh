fdd4d95c2d63deca4600ef8711a99509db728759
Vijay Ramakrishnan <vijayramakrishnan@Vijays-MBP.lan>
2022-09-09 15:37:53 -0700

    Adding support for Go

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ ENV=.env
 
 function trap_ctrlc ()
 {
-    for i in $P1 $P2 $P3 $P4
+    for i in $P1 $P2 $P3 $P4 $P5
     do
         kill -9 -$(ps -o pgid= $i | grep -o '[0-9]*') 
     done
@@ -30,8 +30,17 @@ pip install flask
 python3 $SOURCE/flask/app.py &
 P3=$!
 
+# Go
+FILE=go.work
+if [ ! -f "$FILE" ]; then
+    go work init $SOURCE/gin
+fi
+go run $SOURCE/gin/main.go &
+P4=$!
+
+
 # Setup the proxy
 dotnet run --project $PROXY/proxy.csproj &
-P4=$!
+P5=$!
 
 wait
